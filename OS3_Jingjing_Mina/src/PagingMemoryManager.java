a1e52fb20f519d7f7b2b0bd89d902306ad569373
Jingjing <rong22j@mtholyoke.edu>
2014-11-22 16:33:21 -0500

    - Optimized paging method and fixed more buggggss

--- OS3_Jingjing_Mina/src/PagingMemoryManager.java
+++ OS3_Jingjing_Mina/src/PagingMemoryManager.java
@@ -1,49 +1,67 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
+/**
+ * This handles memory allocation in paging mode
+ * @author Jingjing and Mina
+ *
+ */
 public class PagingMemoryManager extends MemoryManager {
 
 	// initial data structure for tracking, we can discuss what to use for the
 	// algorithm we want
-	private ArrayList<Process> activeProcesses;
-	
+	private HashMap<Integer, Process> activeProcessesMap;
+
 	// memory simulation
 	private int memorySize;
 	private int totalPageNumber;
 	private Page[] pages;
-	
+
 	// keeping track of pages in use
 	private ArrayList<Page> pagesInUse;
 
 	// keeping track of errors
 	private int totalInteralFragmentation = 0;
 	private int noMemoryNumber = 0;
-	
-	// this should always be 0 since it is fixed size paging, there is no external fragmentation
-	private int extFragNumber = 0;
 
+	// this should always be 0 since it is fixed size paging, there is no
+	// external fragmentation
+	private final int extFragNumber = 0;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param bytes
+	 *            total size of memory available
+	 */
 	public PagingMemoryManager(int bytes) {
-		
-        this.memorySize = bytes;
+
+		this.memorySize = bytes;
+
+		// calculate maximum possible number of pages
 		int leftOverSize = bytes % 32;
 		totalInteralFragmentation += leftOverSize;
 		this.totalPageNumber = (bytes - leftOverSize) / 32;
+
+		// initialize pages
 		this.pages = new Page[totalPageNumber];
-		
+
 		// initializing that all pages are not used
-		for(int i = 0; i < totalPageNumber; i++)
-		{
-			Page p = new Page(i,0);
-			// set it to a process that is does not exists 
+		for (int i = 0; i < totalPageNumber; i++) {
+			Page p = new Page(i, 0);
+
+			// set it to a process that is does not exists, this is for
+			// preventing null access exception
 			p.setAssignedProcess(new Process(-1, -1));
-			pages[i] = p;			
+
+			pages[i] = p;
 		}
-		
+
 		// so far no page is in use
 		this.pagesInUse = new ArrayList<Page>();
-		
+
 		// and no process is active
-		this.activeProcesses = new ArrayList<Process>();	
+		this.activeProcessesMap = new HashMap<Integer, Process>();
 	}
 
 	/**
@@ -55,151 +73,160 @@ public class PagingMemoryManager extends MemoryManager {
 
 		// allocate this many bytes to the process with this id
 		Process newProcess = new Process(pid, bytes);
-		
+
 		System.out.println("allocating process" + pid + " size " + bytes);
 
-		// TODO allocate memoroy
+		// allocate memoroy, MATH.ceil stuff behaving weird, using direct
+		// calculation
 		int sizeOfLastPage = bytes % 32;
 		int sizeNeededToFill = (32 - sizeOfLastPage);
 		int numPagesNeeded = 0;
-		if(sizeOfLastPage != 0)
-		{
-		 numPagesNeeded = (bytes + sizeNeededToFill)/32;
-		}else
-		{
-		 numPagesNeeded = (bytes)/32;
+		if (sizeOfLastPage != 0) {
+			numPagesNeeded = (bytes + sizeNeededToFill) / 32;
+		} else {
+			numPagesNeeded = (bytes) / 32;
 		}
-		System.out.println("numPageNeeded = " + numPagesNeeded);
 
+		System.out.println("numPageNeeded = " + numPagesNeeded);
 
 		// check if sum of used pages and needed pages is over limit
 		if (pagesInUse.size() + numPagesNeeded <= totalPageNumber) {
-			
-			activeProcesses.add(newProcess);
 
-			System.out.println("allocating pages for process : " + newProcess.getId());
+			// all pages needed will be found after the above check
+			activeProcessesMap.put(pid, newProcess);
+
+			System.out.println("allocating pages for process : "
+					+ newProcess.getId());
+
 			// initialize the pages array for the process to record
 			Page[] pagesForThisProcess = new Page[numPagesNeeded];
-			
+
 			// look for a page for each page needed
-			for(int i = 0; i < numPagesNeeded; i++)
-			{
+			for (int i = 0; i < numPagesNeeded; i++) {
 				int index = findNextAvailablePage();
-				if(index == -1)
-				{
-					System.out.println("something wrong either with the size check, or the find method..at this point pages should be enough but they are not");
-					noMemoryNumber ++;
-					return -1;
-				}else
-				{
-					// Found a page now
-					System.out.println("found page " + index + "for process" + newProcess.getId());
-					Page p = pages[index];
-					if( i == numPagesNeeded -1)
-					{
-					    p.setUsedSize(sizeOfLastPage);
-					    totalInteralFragmentation += sizeNeededToFill;
-					    newProcess.setIntFragInPaging(sizeNeededToFill);
-					}else
-					{
-						p.setUsedSize(32);
-					}
-					
-					p.setAssignedProcess(newProcess);
-					p.setVirtualPosition(i);
-					pagesForThisProcess[i] = p;		
-					pagesInUse.add(p);
+
+				// Found a page now
+				System.out.println("found page " + index + "for process"
+						+ newProcess.getId());
+				Page p = pages[index];
+
+				// if this is the last page, it might not be fully utilized
+				if (i == numPagesNeeded - 1) {
+					p.setUsedSize(sizeOfLastPage);
+					totalInteralFragmentation += sizeNeededToFill;
+					newProcess.setIntFragInPaging(sizeNeededToFill);
+				} else {
+
+					// previous pages should all be using 32 bytes
+					p.setUsedSize(32);
 				}
-				
+
+				// let the page remember it's new fields: process, physical
+				// location, and virtual location
+				p.setAssignedProcess(newProcess);
+				p.setVirtualPosition(i);
+				pagesForThisProcess[i] = p;
+
+				// mark this page as in use
+				pagesInUse.add(p);
 			}
+
+			// record the list of pages in the process
 			newProcess.setPages(pagesForThisProcess);
 			return 1;
-			
+
 		} else {
-			
+
+			// used page + pages needed > total pages, not enough memory for
+			// this process
 			System.out.println("Not enough pages");
-			noMemoryNumber ++;
+			noMemoryNumber++;
 			return -1;
 		}
 	}
-	
+
 	/**
 	 * Find the first available page and return its index
-	 * @return the page postion, -1 if not found
+	 * 
+	 * @return the page position, -1 if not found
 	 */
-	private int findNextAvailablePage()
-	{
-		for(int i = 0; i < totalPageNumber; i++)
-		{
-			if(!pages[i].isUsed())
-			{
+	private int findNextAvailablePage() {
+		for (int i = 0; i < totalPageNumber; i++) {
+			if (!pages[i].isUsed()) {
 				return i;
 			}
 		}
-		
+
 		// if not returned at this point, then all pages are being used
-		System.out.println("tried to find next available page but did not find one");
+		System.out
+				.println("tried to find next available page but did not find one");
 		return -1;
 	}
-	 
 
+	/**
+	 * remove a process with a given ID and free up it's memory
+	 * 
+	 * @param int pid: the process's ID
+	 * @return int: 1 if successful, -1 if otherwise.
+	 */
 	public int deallocate(int pid) {
 		System.out.println("deleting process" + pid);
 		boolean success = false;
-		int intFragToBeFreed = 0;
-		for(int i = 0; i< pages.length; i++)
-		{
-			Page page = pages[i];
-			Process process = page.getAssignedProcess();
-			
-			if(process.getId() == pid)
-			{
-				// this page needs to be freed
-				page.free();
-				pagesInUse.remove(page);
-				System.out.println("removed page" + page.getPhysicalPosition());
-				activeProcesses.remove(process);
-				intFragToBeFreed = process.getIntFragInPaging();
+
+		// check if this process is really assigned anything in memory
+		if (activeProcessesMap.containsKey(pid)) {
+
+			// gather info needed
+			Process targetProcess = activeProcessesMap.remove(pid);
+			Page[] targetPages = targetProcess.getPages();
+			int intFragToBeFreed = targetProcess.getIntFragInPaging();
+
+			// free all the pages this process is using
+			for (int i = 0; i < targetPages.length; i++) {
+				Page ithPage = pages[i];
+				ithPage.free();
+				success = pagesInUse.remove(ithPage);
+
+				System.out.println("removed page"
+						+ ithPage.getPhysicalPosition());
 			}
+
+			// then free up the internal fragmentation this process caused
+			totalInteralFragmentation -= intFragToBeFreed;
+
+			System.out.println("int frag is now " + totalInteralFragmentation
+					+ ", after removing " + intFragToBeFreed);
+		} else {
+
+			// this process is not active in memory
+			System.out.println("Trying to remove process" + pid
+					+ ", this process is not active yet!");
 			
-			
+			// return not successful
+			return -1;
 		}
 		
-		totalInteralFragmentation -= intFragToBeFreed;
-		System.out.println("int frag is now " + totalInteralFragmentation + ", after removing " + intFragToBeFreed);
 		return success == true ? 1 : -1;
-		// deallocate memory allocated to this process
-		// return 1 if successful, -1 otherwise with an error message
-		// TODO How to return an int AND an error message -.-||| I can print out?
-
 	}
 
-	private void printFreePageNumbers() {
-		String s = "";
-		for (Page p : pages) {
-			if (!p.isUsed()) {
-				s += p.getPhysicalPosition() + ", ";
-			}
-		}
-		System.out.println(s);
-	}
-
+	/**
+	 * Print the current memory state
+	 */
 	public void printMemoryState() {
-		// print out current state of memory
-		// the output will depend on the memory allocator being used.
 
 		// PAGING Example:
 		// Memory size = 1024 bytes, total pages = 32
-		System.out.println("============================Printing==================================");
+		System.out
+				.println("============================Printing==================================");
 		System.out.println("Meory size = " + memorySize + ", total pages = "
 				+ totalPageNumber);
 		// allocated pages = 6, free pages = 26
 		System.out.println("allocated pages = " + pagesInUse.size()
 				+ ", free pages = " + (totalPageNumber - pagesInUse.size()));
 		// There are currently 3 active process
-		System.out.println("There are currently " + activeProcesses.size()
+		System.out.println("There are currently " + activeProcessesMap.size()
 				+ " active process(es)");
-		
+
 		System.out.println("Free Page list:");
 		printFreePageNumbers();
 		// 2, 6, 7, 8, 9, 10, 11, 12...
@@ -218,13 +245,28 @@ public class PagingMemoryManager extends MemoryManager {
 				+ extFragNumber);
 	}
 
+	/**
+	 * Helper for printing active processes info and it's memory usage info
+	 */
 	private void printProcessListAndPageInfo() {
-		for (Process process : activeProcesses) {
-			System.out.println("Process id = " + process.getId() + ", size = "
-					+ process.getSize() + ", number of pages"
-					+ process.getPages().length);
 
-			process.printPagesInfo();
+		// for each active process in the map print process info, and page info
+		for (int key : activeProcessesMap.keySet()) {
+			Process p = activeProcessesMap.get(key);
+			p.printPagesInfo();
 		}
+	}
+
+	/**
+	 * Helper for printing free page locations
+	 */
+	private void printFreePageNumbers() {
+		String s = "";
+		for (Page p : pages) {
+			if (!p.isUsed()) {
+				s += p.getPhysicalPosition() + ", ";
+			}
+		}
+		System.out.println(s);
 	}
 }
--- OS3_Jingjing_Mina/src/Process.java
+++ OS3_Jingjing_Mina/src/Process.java
@@ -1,9 +1,23 @@
+/**
+ * The process object records information regarding a process
+ * @author Jingjing and Mina
+ *
+ */
 public class Process {
 
+	// process's unique ID number
 	private int id;
+	
+	// total memory that is process is taking
 	private int size;
+	
+	// segments that are allocated to this
 	private Segment[] segments;
+	
+	// pages that are allocated to this
 	private Page[] pages;
+	
+	// amount of intFragmentation
 	private int intFragInPaging;
 
 	/**
@@ -21,15 +35,28 @@ public class Process {
 		this.pages = pages;
 	}
 
+	/**
+	 * Constructor 
+	 * @param id 
+	 * @param size
+	 */
 	public Process(int id, int size) {
 		this.id = id;
 		this.size = size;
 	}
 
+	/**
+	 * gets the segments
+	 * @return segments
+	 */
 	public Segment[] getSegments() {
 		return this.segments;
 	}
 
+	/**
+	 * sets the segments
+	 * @param segments array of Segment
+	 */
 	public void setSegments(Segment[] segments) {
 		this.segments = segments;
 	}
@@ -65,11 +92,18 @@ public class Process {
 	}
 
 	/**
+	 * Process id size number of pages
 	 * Virt Page 0 -> Phys Page 0 used: 32 bytes Virt Page 1 -> Phys Page 3
 	 * used: 32 bytes Virt Page 2 -> Phys Page 4 used: 31 bytes
 	 */
 	public void printPagesInfo() {
+		
+		// print process info
+		System.out.println("Process id = " + id + ", size = "
+				+ size + ", number of pages"
+				+ pages.length);
 
+		// print each page info
 		for (int i = 0; i < pages.length; i++) {
 			Page p = pages[i];
 			System.out.println("Virt Page " + i + " -> Phys Page"
