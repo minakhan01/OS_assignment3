786e3c0d4e6177ab53fe3044d3738b02d7bc6816
Jingjing <rong22j@mtholyoke.edu>
2014-11-22 11:46:08 -0500

    hpc

---
+++ OS3_Jingjing_Mina/src/HolePositionComparator.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+
+
+public class HolePositionComparator implements Comparator<Hole>{
+	
+	@Override
+	public int compare(Hole hole1, Hole hole2) {
+		// TODO Auto-generated method stub
+		Integer position1=Integer.valueOf(hole1.getStartingPos());
+		Integer position2=Integer.valueOf(hole2.getStartingPos());
+		return position1.compareTo(position2);
+	}
+
+}
